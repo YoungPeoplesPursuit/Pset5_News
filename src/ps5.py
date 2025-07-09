d28f58723291a54ca5dae66817db690069f323b1
Veritas-Lux <gaotd2008@gmail.com>
2025-07-08 22:24:07 -0500

    Debugged time triggers and wrote reading triggers function. tested and debugged those functions. i think it's about to work

--- src/ps5.py
+++ src/ps5.py
@@ -145,8 +145,11 @@ class TimeTrigger(Trigger):
     def __init__(self, stringtime):
         format_string = "%d %b %Y %H:%M:%S"
         dateformat = datetime.strptime(stringtime, format_string)
+
         est_timezone = pytz.timezone('US/Eastern')  # Define EST timezone
-        self.time = dateformat.replace(tzinfo=est_timezone) #the news is in eastern time so make everything EST
+        # To force it to be UTC-5 as implied by "EST" in the problem statement:
+        est_offset = pytz.FixedOffset(-300)  # -300 minutes = -5 hours = UTC-5
+        self.time = dateformat.replace(tzinfo=est_offset)  # Make it aware with a fixed UTC-5 offset
 
 # Constructor:
 #        Input: Time has to be in EST and in the format of "%d %b %Y %H:%M:%S".
@@ -160,7 +163,11 @@ class BeforeTrigger(TimeTrigger):
 
     def evaluate(self, story):
         storytime = story.get_pubdate()
-        return storytime < self.time
+        # Convert both self.time and storytime_aware to UTC for comparison
+        trigger_time_utc = self.time.astimezone(pytz.utc)
+        story_time_utc = storytime.astimezone(pytz.utc)
+
+        return story_time_utc < trigger_time_utc
 
 class AfterTrigger(TimeTrigger):
     def __init__(self, stringtime):
@@ -168,7 +175,10 @@ class AfterTrigger(TimeTrigger):
 
     def evaluate(self, story):
         storytime = story.get_pubdate()
-        return storytime > self.time
+        trigger_time_utc = self.time.astimezone(pytz.utc)
+        story_time_utc = storytime.astimezone(pytz.utc)
+
+        return story_time_utc > self.time
 
         # COMPOSITE TRIGGERS
 
@@ -208,10 +218,12 @@ def filter_stories(stories, triggerlist):
 
     Returns: a list of only the stories for which a trigger in triggerlist fires.
     """
-    # TODO: Problem 10
-    # This is a placeholder
-    # (we're just returning all the stories, with no filtering)
-    return stories
+    triggeredList = []
+    for story in stories:
+        for trigger in triggerlist:
+            if trigger.evaluate(story):
+                triggeredList.append(story)
+    return triggeredList
 
 
 
@@ -219,6 +231,7 @@ def filter_stories(stories, triggerlist):
 # User-Specified Triggers
 #======================
 # Problem 11
+
 def read_trigger_config(filename):
     """
     filename: the name of a trigger configuration file
@@ -235,13 +248,90 @@ def read_trigger_config(filename):
         if not (len(line) == 0 or line.startswith('//')):
             lines.append(line)
 
-    # TODO: Problem 11
-    # line is the list of lines that you need to parse and for which you need
-    # to build triggers
+    # create dictionaries mapping words to functions
+    triggerkeys1arg = {'TITLE': TitleTrigger, 'DESCRIPTION' : DescriptionTrigger, 'NOT' : NotTrigger, 'BEFORE' : BeforeTrigger, 'AFTER': AfterTrigger}
+    triggerkeys2arg = {'AND': AndTrigger, 'OR': OrTrigger}
+    triggerkeys = {} #where we store the triggers created
+    addedtriggers = [] #where we add the triggers to the list to return
+    for line in lines:
+        linelist = line.split(',')
+        if linelist[0] == 'ADD': #if is ADD simply add triggers from dictionary to list to return
+            for i in range(1, len(linelist)):
+                triggername = linelist[i]
+                if triggername in triggerkeys:
+                    addedtriggers.append(triggerkeys[triggername])
+        else: #find the function corresponding to word, add triggerkeys key as trigger name and value as function with parameters plugged in
+            triggername = linelist[0]
+            triggertype = linelist[1]
+            if triggertype in triggerkeys1arg:
+                function = triggerkeys1arg[triggertype]
+                triggerkeys[triggername] = function(linelist[2])
+            else:
+                function = triggerkeys2arg[triggertype]
+                triggerkeys[triggername] = function(linelist[2], linelist[3])
 
-    print(lines) # for now, print it so you see what it contains!
+    return addedtriggers
+'''
 
 
+    print("\n--- Reading Triggers Configuration ---")  # Debug print
+    for line in lines:
+        print(f"Processing line: '{line}'")  # Debug print
+        linelist = [item.strip() for item in line.split(',')]
+
+        trigger_identifier = linelist[0]
+
+        if trigger_identifier == 'ADD':
+            print(f"  --> Handling ADD command.")  # Debug print
+            for i in range(1, len(linelist)):
+                trigger_name_to_add = linelist[i]
+                if trigger_name_to_add in triggerkeys:
+                    addedtriggers.append(triggerkeys[trigger_name_to_add])
+                    print(
+                        f"    Added '{trigger_name_to_add}' (type: {type(triggerkeys[trigger_name_to_add])}) to final list.")  # Debug print
+                else:
+                    print(
+                        f"    Warning: Trigger '{trigger_name_to_add}' not found in defined triggers for ADD command.")  # Debug print
+        else:  # Defines a new trigger
+            trigger_name = trigger_identifier
+            trigger_type = linelist[1]
+            print(f"  --> Defining trigger '{trigger_name}' of type '{trigger_type}'.")  # Debug print
+
+            if trigger_type in triggerkeys1arg:
+                constructor_func = triggerkeys1arg[trigger_type]
+                arg = linelist[2]
+                trigger_obj = constructor_func(arg)  # Call the class constructor
+                triggerkeys[trigger_name] = trigger_obj
+                print(f"    Defined '{trigger_name}' as {type(trigger_obj)} with arg '{arg}'.")  # Debug print
+            elif trigger_type in triggerkeys2arg:
+                constructor_func = triggerkeys2arg[trigger_type]
+                arg1_name = linelist[2]
+                arg2_name = linelist[3]
+
+                if arg1_name in triggerkeys and arg2_name in triggerkeys:
+                    arg1_obj = triggerkeys[arg1_name]
+                    arg2_obj = triggerkeys[arg2_name]
+                    trigger_obj = constructor_func(arg1_obj, arg2_obj)  # Call the class constructor
+                    triggerkeys[trigger_name] = trigger_obj
+                    print(
+                        f"    Defined '{trigger_name}' as {type(trigger_obj)} with args '{arg1_name}' ({type(arg1_obj)}) and '{arg2_name}' ({type(arg2_obj)}).")  # Debug print
+                else:
+                    print(f"    Error: Dependent triggers for '{trigger_name}' ({arg1_name}, {arg2_name}) not found. "
+                          f"Ensure they are defined before composite triggers.")  # Debug print
+            else:
+                print(f"    Error: Unknown trigger type '{trigger_type}' found in configuration file.")  # Debug print
+
+    print("\n--- Final Triggerkeys Dictionary ---")  # Debug print
+    for name, obj in triggerkeys.items():
+        print(f"  '{name}': {type(obj)}")
+
+    print("\n--- Final AddedTriggers List ---")  # Debug print
+    for i, obj in enumerate(addedtriggers):
+        print(f"  Index {i}: {type(obj)}")
+    print("------------------------------------\n")
+
+    return addedtriggers
+'''
 
 SLEEPTIME = 120 #seconds -- how often we poll
 
@@ -249,15 +339,15 @@ def main_thread(master):
     # A sample trigger list - you might need to change the phrases to correspond
     # to what is currently in the news
     try:
-        t1 = TitleTrigger("election")
+        t1 = TitleTrigger("Elon Musk")
         t2 = DescriptionTrigger("Trump")
-        t3 = DescriptionTrigger("Clinton")
+        t3 = DescriptionTrigger("tariff")
         t4 = AndTrigger(t2, t3)
         triggerlist = [t1, t4]
 
         # Problem 11
-        # TODO: After implementing read_trigger_config, uncomment this line 
-        # triggerlist = read_trigger_config('triggers.txt')
+
+        triggerlist = read_trigger_config('triggers.txt')
         
         # HELPER CODE - you don't need to understand this!
         # Draws the popup window that displays the filtered stories
